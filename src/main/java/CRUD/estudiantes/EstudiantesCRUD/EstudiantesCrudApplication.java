0de623edb7d564d96429589a4be516ef1506186d
nelsonenriquez <nenriquezp@miumg.edu.gt>
2023-06-12 23:26:27 -0600

    Iniciando Proyecto y publicandolo en GitHub

---
+++ src/main/java/CRUD/estudiantes/EstudiantesCRUD/EstudiantesCrudApplication.java
@@ -0,0 +1,13 @@
+package CRUD.estudiantes.EstudiantesCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudiantesCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudiantesCrudApplication.class, args);
+	}
+
+}
